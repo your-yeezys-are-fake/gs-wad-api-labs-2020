dc6e0c1e609c7062c5420386c7f9da6cff0b9195
Georgia Swindley <62120557+your-yeezys-are-fake@users.noreply.github.com>
2020-11-26 16:21:37 +0000

    added routing for add,update,delete movie

--- node-lab-1/movies-api/index.js
+++ node-lab-1/movies-api/index.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
+import bodyParser from 'body-parser';
 
 dotenv.config();
 
@@ -8,7 +9,10 @@ const app = express();
 
 const port = process.env.PORT;
 
+//configure body-parser
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 app.use('/api/movies', moviesRouter);
 
 app.listen(port, () => {

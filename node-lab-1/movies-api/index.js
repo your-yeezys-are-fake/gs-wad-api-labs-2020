77a53133481da847bf8ff8dec0a6a017625bc733
Georgia Swindley <62120557+your-yeezys-are-fake@users.noreply.github.com>
2020-12-04 13:15:47 +0000

    Week 9 Included Mongoose

--- node-lab-1/movies-api/index.js
+++ node-lab-1/movies-api/index.js
@@ -2,8 +2,14 @@ import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
 import bodyParser from 'body-parser';
+import './db';
+import {loadUsers} from './seedData'
 
 dotenv.config();
+
+if (process.env.SEED_DB) {
+  loadUsers();
+}
 
 const errHandler = (err, req, res, next) => {
   /* if the error in development then send stack trace to display whole error,
